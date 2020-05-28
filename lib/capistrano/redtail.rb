9c369ce002fa3bd6d11b94381eb91a9c655f7dd3
Colin Rice <colinrice07@gmail.com>
2020-05-27 20:07:25 -0700

    Remove assets file from loader

--- lib/capistrano/redtail.rb
+++ lib/capistrano/redtail.rb
@@ -1,4 +1,3 @@
-load File.expand_path('../tasks/assets.rake', __FILE__)
 load File.expand_path('../tasks/environment.rake', __FILE__)
 load File.expand_path('../tasks/logs.rake', __FILE__)
 load File.expand_path('../tasks/unicorn.rake', __FILE__)
