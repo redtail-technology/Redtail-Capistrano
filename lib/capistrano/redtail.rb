069e95424dfabfce00a578eb7b3118a0705249b1
Colin Rice <colinrice07@gmail.com>
2020-05-27 20:53:15 -0700

    Revert "Making sure to invoke multiple times"
    This reverts commit 35457a26ff0a916e1d79a2bc6210c54907d9c205.

--- lib/capistrano/redtail.rb
+++ lib/capistrano/redtail.rb
@@ -1,3 +1,4 @@
+load File.expand_path('../tasks/assets.rake', __FILE__)
 load File.expand_path('../tasks/environment.rake', __FILE__)
 load File.expand_path('../tasks/logs.rake', __FILE__)
 load File.expand_path('../tasks/unicorn.rake', __FILE__)
